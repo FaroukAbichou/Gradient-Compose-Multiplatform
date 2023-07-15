44d304c3545f8d5541d79575673ab1bff5bd8f89
Farouk <117727520+Farouk-abichou@users.noreply.github.com>
2023-07-16 00:08:27 +0100

    - Some Changes

--- composeApp/src/commonMain/kotlin/org/zold/app/GrPlayGround.kt
+++ composeApp/src/commonMain/kotlin/org/zold/app/GrPlayGround.kt
@@ -4,18 +4,12 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.ExperimentalTextApi
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -28,7 +22,6 @@ fun GrPlayGround() {
     }
 }
 
-@OptIn(ExperimentalTextApi::class)
 @Composable
 fun AnimationLayout() {
 
