d854ab80671489978a8c7535becdff615ee2ee8d
Farouk-abichou <117727520+Farouk-abichou@users.noreply.github.com>
2023-08-04 18:32:12 +0100

    - Showing Off

--- app/src/main/java/com/example/composegradient/CustomShaderBrush.kt
+++ app/src/main/java/com/example/composegradient/CustomShaderBrush.kt
@@ -1,10 +1,12 @@
 package com.example.composegradient
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -18,12 +20,19 @@ import com.example.composegradient.rainbowbox.GradyBox
 @Preview
 fun CustomShaderBrush() {
     Box(
-        modifier = Modifier.background(Color.Black),
+        modifier = Modifier,
         contentAlignment = androidx.compose.ui.Alignment.Center
     ) {
         GradyBox(
             modifier = Modifier
                 .fillMaxSize()
+        )
+        Text(
+            "Farouk Abichou",
+            modifier = Modifier
+                .background(Color.Transparent)
+                .border(100.dp, Color.White)
+            ,
         )
     }
 }
